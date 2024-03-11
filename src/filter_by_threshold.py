9230116bbe61f28d9c41d9e4a48c8928eaecc2d9
alguoo314 <76114178+alguoo314@users.noreply.github.com>
2024-03-11 10:48:07 -0400

    Create filter_by_threshold.py

---
+++ src/filter_by_threshold.py
@@ -0,0 +1,39 @@
+import sys
+
+threshold = float(sys.argv[1])
+
+count = {}
+linecount = {}
+linesupport = {}
+lines = []
+
+for line in sys.stdin:
+    line = line.strip()
+    lines.append(line)
+
+for j in range(len(lines)):
+    fields = lines[j].split("\t")
+    if fields[2] == "transcript":
+        f = fields[8].split(";")
+        for item in f:
+            if item.startswith("read_num="):
+                count[int(item.split("=")[1])] = count.get(int(item.split("=")[1]), 0) + 1
+                linecount[j] = int(item.split("=")[1])
+            elif item.startswith("transcript_support="):
+                linesupport[j] = float(item.split("=")[1])
+
+thresh = sum(i * count[i] for i in count) * threshold
+min_count = 0
+n = 0
+
+for i in count:
+    n += i * count[i]
+    if n > thresh:
+        min_count = i
+        break
+
+print("#gff\n#produced by NIFFLR\n#min read count =", min_count)
+
+for j in range(len(lines)):
+    if linecount.get(j, 0) > min_count or linesupport.get(j, 0) > 0.85:
+        print(lines[j])

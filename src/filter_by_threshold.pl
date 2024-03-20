e27342b8578b710264df8d565fcd135db0c646ca
Aleksey Zimin <alekseyz@salz3.idies.jhu.edu>
2024-03-20 10:53:42 -0400

    added spanning read number to filter by threshold

--- src/filter_by_threshold.pl
+++ src/filter_by_threshold.pl
@@ -6,12 +6,17 @@ while($line=<STDIN>){
 }
 my $current_count;
 my $current_support;
+my $minjcov;
+my %maxjcov=();
 for($j=0;$j<=$#lines;$j++){
   @F=split(/\t/,$lines[$j]);
   if($F[2] eq "transcript"){
     @f=split(/;/,$F[8]);
     for($i=0;$i<=$#f;$i++){
-      if($f[$i] =~ /^read_num=/){
+      if($f[$i] =~ /^geneID=/){
+        @ff=split(/=/,$f[$i]);
+        $geneID=$ff[1];
+      }elsif($f[$i] =~ /^read_num=/){
         @ff=split(/=/,$f[$i]);
         $count[int($ff[1])]++;
         $current_count=$ff[1];
@@ -20,10 +25,12 @@ for($j=0;$j<=$#lines;$j++){
         $current_support=$ff[1];
       }elsif($f[$i] =~ /^least_junction_reads_coverage=/){
         @ff=split(/=/,$f[$i]);
+        $maxjcov{$geneID}=$ff[1] if($ff[1]>$maxjcov{$geneID});
         $minjcov=$ff[1];
       }
     }
   }
+  $linemaxjcov[$j]=$maxjcov{$geneID};
   $lineminjcov[$j]=$minjcov;
   $linecount[$j]=$current_count;
   $linesupport[$j]=$current_support;
@@ -44,5 +51,6 @@ for($i=0;$i<$#count;$i++){
 }
 print "#gff\n#produced by NIFFLR\n#min read count = $min_count\n";
 for($j=0;$j<=$#lines;$j++){
-  print $lines[$j],"\n" if(($linecount[$j] > $min_count || $linesupport[$j] > 0.85) && $lineminjcov[$j] > 1);
+  print $lines[$j],"\n" if(($linecount[$j] > $min_count || $linesupport[$j] > 0.9) && ($lineminjcov[$j]>1 ||  $linemaxjcov[$j]==1 || $lineminjcov[$j] == -1));
+  #print $lines[$j],"\n" if(($linecount[$j] > $min_count || $linesupport[$j] > 0.9));
 }

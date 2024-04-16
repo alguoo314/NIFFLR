333938ad10ea02b08ab6d5d19122f921a771ed68
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2024-04-16 08:43:52 -0400

    added psa_min for speed, B is 40, cosmetic changes

--- src/nifflr.sh
+++ src/nifflr.sh
@@ -9,7 +9,7 @@ OUTPUT_PREFIX="output"
 DISCARD_INTERM=false
 QUANT=false
 JF_THREADS=16
-BASES=50
+BASES=40
 MER=11
 GAP_OVERLAP_ALLOWANCE=15
 if tty -s < /dev/fd/1 2> /dev/null; then
@@ -161,7 +161,7 @@ fi
 if [ ! -e nifflr.alignment.success ];then
   log "Running jf_aligner to align between the reads and the reference exons, folowed by finding the best path through the exons in each read" && \
   SIZE=$(grep -v ">" $OUTPUT_PREFIX.exons.fna | awk '{sum += length} END {print sum}') && \
-  zcat -f $INPUT_READS | fastqToFasta.pl |psa_aligner -t $JF_THREADS -B $BASES -m $MER -s $SIZE -q /dev/stdin -r $OUTPUT_PREFIX.exons.fna --coords /dev/stdout | \
+  zcat -f $INPUT_READS | fastqToFasta.pl |psa_aligner -t $JF_THREADS -B $BASES -m $MER --psa-min $MER -s $SIZE -q /dev/stdin -r $OUTPUT_PREFIX.exons.fna --coords /dev/stdout | \
   tee alignments.txt | \
   $MYPATH/majority_vote.py | \
   $MYPATH/find_path.py -o $OUTPUT_PREFIX.best_paths.fasta.tmp && \
@@ -198,7 +198,7 @@ if [ ! -e nifflr.count.success ];then
   python $MYPATH/count_junction_coverage.py -i $OUTPUT_PREFIX.sorted.good_output.gff -s $OUTPUT_PREFIX.exon_junction_counts.csv -c $OUTPUT_PREFIX.full_exon_junction_counts.csv && \
   python $MYPATH/quantification.py -a $OUTPUT_PREFIX.sorted.good_output.gff -r $OUTPUT_PREFIX.sorted.combined.gff -o $OUTPUT_PREFIX.asm.reads.assigned.gff -c chr_names.txt --single_junction_coverage $OUTPUT_PREFIX.exon_junction_counts.csv --full_junction_coverage $OUTPUT_PREFIX.full_exon_junction_counts.csv && \
   rm $OUTPUT_PREFIX.sorted.combined.gff $OUTPUT_PREFIX.sorted.good_output.gff chr_names.txt $OUTPUT_PREFIX.exon_junction_counts.csv $OUTPUT_PREFIX.full_exon_junction_counts.csv && \
-  filter_by_threshold.pl 0.0075 < $OUTPUT_PREFIX.asm.reads.assigned.gff >  $OUTPUT_PREFIX.asm.reads.assigned.filtered.gff && \
+  $MYPATH/filter_by_threshold.pl 0.0075 < $OUTPUT_PREFIX.asm.reads.assigned.gff >  $OUTPUT_PREFIX.asm.reads.assigned.filtered.gff && \
   touch nifflr.count.success || error_exit "Assembled transcripts quantification failed"
 fi
 

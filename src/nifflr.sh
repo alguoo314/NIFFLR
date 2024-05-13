c9aa059a608a4c1ba4903aae26c1734c085b2d4c
Aleksey Zimin <alekseyz@salz3.idies.jhu.edu>
2024-05-13 08:31:28 -0400

    added two-step operation with reduced k-mer size for unreliable reads

--- src/nifflr.sh
+++ src/nifflr.sh
@@ -160,8 +160,15 @@ if [ ! -e nifflr.alignment.success ];then
   $MYPATH/fastqToFasta.pl |\
   $MYPATH/psa_aligner -t $JF_THREADS -B $BASES -m $MER --psa-min $MER -s $SIZE -q /dev/stdin -r $OUTPUT_PREFIX.exons.fna --coords /dev/stdout | \
   $MYPATH/majority_vote.py | \
-  tee alignments.txt | \
-  $MYPATH/find_path.py -o $OUTPUT_PREFIX.best_paths.fasta.tmp && \
+  tee >(awk '{if($1 ~ /^>/){rn=substr($1,2)}else{print rn" "$2" "$3" "$4" "$8}}' | uniq -D -f 2 |awk '{print $1}' > $OUTPUT.unreliable_reads.txt) | \
+  $MYPATH/find_path.py -o $OUTPUT_PREFIX.best_paths.fasta.$MER.tmp && \
+  zcat -f $INPUT_READS | \
+  $MYPATH/fastqToFasta.pl | \
+  ufasta extract -f $OUTPUT.unreliable_reads.txt |\
+  $MYPATH/psa_aligner -t $JF_THREADS -B $(($BASES+5)) -m $(($MER-1)) --psa-min $(($MER-1)) -s $SIZE -q /dev/stdin -r $OUTPUT_PREFIX.exons.fna --coords /dev/stdout | \
+  $MYPATH/majority_vote.py | \
+  $MYPATH/find_path.py -o $OUTPUT_PREFIX.best_paths.$(($MER-1)).fasta.tmp && \
+  cat <(ufasta extract -v -f unreliable_reads.txt $OUTPUT_PREFIX.best_paths.fasta.$MER.tmp) $OUTPUT_PREFIX.best_paths.$(($MER-1)).fasta.tmp > $OUTPUT_PREFIX.best_paths.fasta.tmp && \
   mv $OUTPUT_PREFIX.best_paths.fasta.tmp $OUTPUT_PREFIX.best_paths.fasta && \
   rm -f nifflr.gtf_generation.success && \
   touch nifflr.alignment.success || error_exit "jf_aligner or majority voting or finding the best path failed. Please see the detailed error messages."
